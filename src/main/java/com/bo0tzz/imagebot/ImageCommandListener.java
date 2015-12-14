23721dbddef11bdd409d1fda7bdcd78b0b432ce9
bo0tzz <btdewilligen@gmail.com>
2015-12-14 01:50:35 +0100

    more testing in prod because I'm a lazy fuck

--- src/main/java/com/bo0tzz/imagebot/ImageCommandListener.java
+++ src/main/java/com/bo0tzz/imagebot/ImageCommandListener.java
@@ -114,6 +114,39 @@ public class ImageCommandListener implements Listener {
                 .document(new InputFile(url))
                 .replyTo(event.getMessage())
                 .build(), ImageBot.bot);
+        } else if (event.getCommand().equals("test1")) {
+            URL url = null;
+            try {
+                url = new URL("http://www.zackpollard.pro/at-screenshots/Og5wgEuc.png");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            event.getChat().sendMessage(SendablePhotoMessage.builder()
+                    .photo(new InputFile(url))
+                    .replyTo(event.getMessage())
+                    .build(), ImageBot.bot);
+            event.getChat().sendMessage(SendablePhotoMessage.builder()
+                    .photo(new InputFile(url))
+                    .replyTo(event.getMessage())
+                    .build(), ImageBot.bot);
+        } else if (event.getCommand().equals("test2")) {
+            URL url1 = null;
+            URL url2 = null;
+            try {
+                url1 = new URL("http://www.zackpollard.pro/at-screenshots/Og5wgEuc.png");
+                url2 = new URL("http://www.zackpollard.pro/at-screenshots/9F_NE7z3.png");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            event.getChat().sendMessage(SendablePhotoMessage.builder()
+                    .photo(new InputFile(url1))
+                    .replyTo(event.getMessage())
+                    .build(), ImageBot.bot);
+            event.getChat().sendMessage(SendablePhotoMessage.builder()
+                    .photo(new InputFile(url2))
+                    .replyTo(event.getMessage())
+                    .build(), ImageBot.bot);
         }
     }
 
